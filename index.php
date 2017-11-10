7ebece1671875a23fcee73a935339b2dc8ac8821
Colin McNamara <coolinmc6@gmail.com>
2017-11-10 10:45:57 -0500

    Paginate save

--- index.php
+++ index.php
@@ -43,18 +43,15 @@ $photos = Photo::find_by_query($sql);
                     <ul class="pager">
 
                         <?php 
-
-                        if($paginate->page_total() > 1) {
-                            if($paginate->has_next()) {
-                                echo "<li class='next'><a href='index.php?page={$paginate->next()}'>Next</a></li>";        
+                            if($paginate->page_total() > 1) {
+                                if($paginate->has_next()) {
+                                    echo "<li class='next'><a href='index.php?page={$paginate->next()}'>Next</a></li>";
+                                }
+                                if($paginate->has_previous()) {
+                                    echo "<li class='previous'><a href='index.php?page={$paginate->previous()}'>Previous</a></li>";
+                                }
                             }
-                            if($paginate->has_previous()) {
-                                echo "<li class='previous'><a href='index.php?page={$paginate->previous()}'>Previous</a></li>";
-                            }
-                        }
-
-
-                         ?>
+                        ?>
                         
                     </ul>
                 </div>     
